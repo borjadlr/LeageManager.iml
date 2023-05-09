5172e4d765d90e5188056b0e8aceb9ec87514365
11nubi <100545830+11nubi@users.noreply.github.com>
2023-05-10 01:11:32 +0200

    BottomPanel

--- src/Presentation/Controllers/BottomPanelController.java
+++ src/Presentation/Controllers/BottomPanelController.java
@@ -1,4 +1,32 @@
 package Presentation.Controllers;
 
-public class BottomPanelController {
+import Presentation.Views.BottomPanelGUI;
+import Presentation.Views.ChangePasswordGUI;
+import Presentation.Views.MainFrameGUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BottomPanelController implements ActionListener {
+
+
+    private static final String CHANGE_PASSWORD = "Change Password";
+    private final BottomPanelGUI bottomPanel;
+    private final ChangePasswordGUI changePasswordGUI;
+    private final MainFrameGUI mainView;
+
+    public BottomPanelController(MainFrameGUI mainView, BottomPanelGUI bottomPanel, ChangePasswordGUI changePasswordGUI){
+        this.bottomPanel = bottomPanel;
+        this.changePasswordGUI = changePasswordGUI;
+        this.mainView = mainView;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       switch (e.getActionCommand()){
+           case CHANGE_PASSWORD:
+               mainView.showCanviContraseña();
+               break;
+       }
+    }
 }
--- src/Presentation/Views/BottomPanelGUI.java
+++ src/Presentation/Views/BottomPanelGUI.java
@@ -1,8 +1,15 @@
 package Presentation.Views;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class BottomPanelGUI extends JPanel {
+
+    private JButton changePassword;
+
+    private static final String INTER = "INTER";
+    private static final String CHANGE_CURRENT_PASSWORD = "Change Current Password";
 
     public BottomPanelGUI() {
         setLayout(new BorderLayout());
@@ -15,11 +22,17 @@ public class BottomPanelGUI extends JPanel {
         downSection.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 5));
 
         // Add a button to the white section to change the current password
-        JButton changePassword = new JButton("Change Current Password");
+        changePassword = new JButton(CHANGE_CURRENT_PASSWORD);
         changePassword.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-        changePassword.setFont(Font.getFont("INTER"));
+        changePassword.setFont(Font.getFont(INTER));
         changePassword.setPreferredSize(new Dimension(180, 30));
         changePassword.setBackground(Color.WHITE);
         downSection.add(changePassword, BorderLayout.EAST);
     }
+
+    public void changePasswordListener(ActionListener actionListener){
+        changePassword.addActionListener(actionListener);
+    }
 }
+
+
