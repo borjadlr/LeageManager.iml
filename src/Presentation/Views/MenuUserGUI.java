defcfca403812f83625c332373be04dc69173d0b
11nubi <100545830+11nubi@users.noreply.github.com>
2023-05-20 13:58:15 +0200

    Test

--- src/Presentation/Views/MenuUserGUI.java
+++ src/Presentation/Views/MenuUserGUI.java
@@ -4,23 +4,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-public class MenuUserGUI extends JPanel{
-
+public class MenuUserGUI extends JPanel {
     private JButton watchMatches;
     private JButton createPlayer;
     private JButton viewAvaiableLeagues;
-    private JButton back;
     private Color backgroundColor;
 
-    public static final String WATCH_MATCHES = "WATCH_MATCHES";
-    public static final String CREATE_PLAYER = "CREATE_PLAYER";
-    public static final String VIEW_AVAIABLE_LEAGUES = "VIEW_AVAIABLE_LEAGUES";
-
-    public static final String BACK_BUTTON = "BACK_BUTTON";
+    private static final String WATCH_MATCHES = "WATCH_MATCHES";
+    private static final String CREATE_PLAYER = "CREATE_PLAYER";
+    private static final String VIEW_AVAIABLE_LEAGUES = "VIEW_AVAIABLE_LEAGUES";
+    //public static final String BACK_BUTTON = "BACK_BUTTON";
 
     public MenuUserGUI() {
 
-        backgroundColor = Color.lightGray;
+        backgroundColor = Color.white;
         setLayout(new GridBagLayout());
         this.setBackground(backgroundColor);
         this.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
@@ -30,7 +27,7 @@ public class MenuUserGUI extends JPanel{
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setBorder(BorderFactory.createEmptyBorder(0, 50, 100, 50));
         panel.setBackground(backgroundColor);
-        panel.setOpaque(true);
+        //panel.setOpaque(true);
 
         // Image panel
         JPanel imagePanel = new JPanel();
@@ -43,29 +40,43 @@ public class MenuUserGUI extends JPanel{
 
         //Title
         JLabel title = new JLabel("User Menu");
-        title.setFont(new Font("Apple Casual", Font.PLAIN, 25));
+        title.setFont(new Font("Apple Casual", Font.PLAIN, 60));
         title.setForeground(Color.black);
         title.setBorder(BorderFactory.createEmptyBorder(0,0,10,0));
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(title);
 
+        panel.add(Box.createVerticalStrut(90));
+
         //Create account
         watchMatches = new JButton("Watch Live Matches");
         watchMatches.setActionCommand(WATCH_MATCHES);
+        watchMatches.setPreferredSize(new Dimension(150, 30));
         watchMatches.setAlignmentX(Component.CENTER_ALIGNMENT);
+        watchMatches.setBorder(BorderFactory.createLineBorder(Color.black, 2));
         panel.add(watchMatches);
+
+        panel.add(Box.createVerticalStrut(15));
 
         //Create New Player
         createPlayer = new JButton("Create New Player");
         createPlayer.setActionCommand(CREATE_PLAYER);
         createPlayer.setAlignmentX(Component.CENTER_ALIGNMENT);
+        createPlayer.setPreferredSize(new Dimension(150, 30));
+        createPlayer.setBorder(BorderFactory.createLineBorder(Color.black, 2));
         panel.add(createPlayer);
+
+        panel.add(Box.createVerticalStrut(15));
 
         //View avaiable matches
         viewAvaiableLeagues = new JButton("View Available Leagues");
         viewAvaiableLeagues.setActionCommand(VIEW_AVAIABLE_LEAGUES);
+        viewAvaiableLeagues.setPreferredSize(new Dimension(150, 30));
         viewAvaiableLeagues.setAlignmentX(Component.CENTER_ALIGNMENT);
+        viewAvaiableLeagues.setBorder(BorderFactory.createLineBorder(Color.black, 2));
         panel.add(viewAvaiableLeagues);
+
+        panel.add(Box.createVerticalStrut(15));
 
         this.add(panel);
 
