7a64db0096dfb2fcf60edf62468bfdc90dc89f1f
borjariva <97692758+borjariva@users.noreply.github.com>
2023-05-23 18:20:49 +0200

    Match

--- src/Business/Entities/Match.java
+++ src/Business/Entities/Match.java
@@ -4,69 +4,78 @@ import java.util.Date;
 
 public class Match {
 
-    private Team team1;
-    private Team team2;
+    private String local;
+    private String visitante;
 
-    private String golesTeam1;
-
-    private String golesTeam2;
-
-    private Date matchDay;
+    private int golesLocal;
+    private int golesVisitante;
+    private int jornada;
     private boolean status;
+    private String nombreLiga;
 
-    public Match(Team team1, Team team2, boolean status) {
-        this.team1 = team1;
-        this.team2 = team2;
+    public Match(String local, String visitante, int golesLocal, int golesVisitante, int jornada, boolean status, String nombreLiga) {
+        this.local = local;
+        this.visitante = visitante;
+        this.golesLocal = golesLocal;
+        this.golesVisitante = golesVisitante;
+        this.jornada = jornada;
         this.status = status;
+        this.nombreLiga = nombreLiga;
     }
 
-    public Team getTeam1() {
-        return team1;
+    public String getLocal() {
+        return local;
     }
 
-    public Team getTeam2() {
-        return team2;
+    public void setLocal(String local) {
+        this.local = local;
     }
 
-    
-
-    public void setTeam1(Team team1) {
-        this.team1 = team1;
+    public String getVisitante() {
+        return visitante;
     }
 
-    public void setTeam2(Team team2) {
-        this.team2 = team2;
+    public void setVisitante(String visitante) {
+        this.visitante = visitante;
+    }
+
+    public int getGolesLocal() {
+        return golesLocal;
+    }
+
+    public void setGolesLocal(int golesLocal) {
+        this.golesLocal = golesLocal;
+    }
+
+    public int getGolesVisitante() {
+        return golesVisitante;
+    }
+
+    public void setGolesVisitante(int golesVisitante) {
+        this.golesVisitante = golesVisitante;
+    }
+
+    public int getJornada() {
+        return jornada;
+    }
+
+    public void setJornada(int jornada) {
+        this.jornada = jornada;
+    }
+
+    public boolean isStatus() {
+        return status;
     }
 
     public void setStatus(boolean status) {
         this.status = status;
     }
 
-    public void setGolesTeam1(String golesTeam1) {
-        this.golesTeam1 = golesTeam1;
+    public String getNombreLiga() {
+        return nombreLiga;
     }
 
-    public void setGolesTeam2(String golesTeam2) {
-        this.golesTeam2 = golesTeam2;
-    }
-
-    public void setMatchDay(Date matchDay) {
-        this.matchDay = matchDay;
-    }
-
-    public Date getMatchDay() {
-        return matchDay;
-    }
-
-    public String getGolesTeam1() {
-        return golesTeam1;
-    }
-
-    public String getGolesTeam2() {
-        return golesTeam2;
-    }
-
-    public boolean isStatus() {
-        return status;
+    public void setNombreLiga(String nombreLiga) {
+        this.nombreLiga = nombreLiga;
     }
 }
